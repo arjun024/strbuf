41033fba863f646deaf391a50fc750431b453def
Arjun Sreedharan <arjun024@gmail.com>
2016-04-08 19:13:51 +0530

    size_t print using %zu
    fixes issues/2
    Signed-off-by: Arjun Sreedharan <arjun024@gmail.com>

--- test.c
+++ test.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	struct strbuf name = STRBUF_INIT;
 	int value = 701;
-	
+
 	strbuf_addstr(&name, "a string");
 	strbuf_addf(&name, " and a formatted string: param=%d", value);
 
 	printf("1. %s\n", name.buf);
-	printf("2. %d\n", name.len);
+	printf("2. %zu\n", name.len);
 
 	strbuf_release(&name);
 	return 0;
